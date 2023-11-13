ee82d8f68634d972f38b65252b645bce7884c2e0
KEVIN JOSEPH HENDRY <88765024+kevin-j-h@users.noreply.github.com>
2023-11-13 18:12:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 function login() {
-    event.preventDefault(); // Prevents the default form submission
-
-    // Get the values entered by the user
+    event.preventDefault();
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-
-    // Replace this with your actual login logic using AJAX/fetch to send data to a server
-    // For demonstration purposes, we'll use a simple alert
     if (username === "leo messi" && password === "goat") {
         window.location.href = "home.html";
     } else {
